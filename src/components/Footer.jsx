ac134d1f6e55280d2a441c851f6a21cfc4502de0
iamrawat21 <90498266+iamrawat21@users.noreply.github.com>
2022-10-09 13:10:21 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒDevansh {2022}</p>
+    </footer>
+  );
+}
+
+export default Footer;
